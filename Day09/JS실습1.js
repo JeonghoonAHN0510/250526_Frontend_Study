1712de132ea495be157b0ad63d1668387b240f33
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-06-09 16:03:02 +0900

    Day09 실습 END

--- Day09/JS실습1.js
+++ Day09/JS실습1.js
@@ -1,13 +1,17 @@
-console.log( 130 )
-console.log( 1 )
-console.log( 2 )
-console.log( "카테고리명" )
-console.log( "지점명" )
-console.log( "쿠폰번호" )
-console.log( "전화번호" )
-console.log( "영수증 발급여부(true/false)" )
-console.log( "얼음 선택(true/false)" )
-console.log( "쿠폰 사용여부(true/false)" )
-console.log( "유료 옵션들" )
-console.log( "카테고리내 음료들" )
-console.log( "카테고리들" )
+console.log("[숫자 자료]");
+console.log( 130 );
+console.log( 1 );
+console.log( 2 );
+console.log("[문자열 자료]");
+console.log( "카테고리명" );
+console.log( "지점명" );
+console.log( "쿠폰번호" );
+console.log( "전화번호" );
+console.log("[불리언 자료]");
+console.log( "영수증 발급여부(true/false)" );
+console.log( "얼음 선택(true/false)" );
+console.log( "쿠폰 사용여부(true/false)" );
+console.log("[배열 자료]");
+console.log( "유료 옵션들" );
+console.log( "카테고리내 음료들" );
+console.log( "카테고리들" );
