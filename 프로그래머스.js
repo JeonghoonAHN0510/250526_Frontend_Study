a7ace2e664aeff685f634ec7dc0e94c3268ea269
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-06-18 18:18:31 +0900

    Day16 학원 끝

---
+++ 프로그래머스.js
@@ -0,0 +1,14 @@
+function solution(a, b, c, d) {
+    let answer = 0;
+    let array = [];
+    array.push(a);
+    array.push(b);
+    array.push(c);
+    array.push(d);
+    for (let i = 0; i <= array.length - 1; i++){
+        
+    }
+    
+    
+    return answer;
+}
