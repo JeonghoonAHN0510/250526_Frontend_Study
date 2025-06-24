fde13f26f9c297b8ab7e0bcb15ef14e48499c02e
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-06-24 13:22:01 +0900

    Day20 4교시

--- Day20/stock.js
+++ Day20/stock.js
@@ -1,6 +1,6 @@
 //=============================제품 등록 함수=============================
 // 실행조건 : 제품 등록 버튼이 눌렸을 때
-function StockRegistration(){
+function StockRegistration() {
     console.log('----StockRegistration----');
     // 1. 무엇을 : 입력받은 값들을
     // 1-1. 입력 마크업 객체 가져오기
@@ -8,24 +8,24 @@ function StockRegistration(){
     const stockTypeInput = document.querySelector('#stockTypeInput');   // console.log( stockTypeInput );
     const stockCountInput = document.querySelector('#stockCountInput'); // console.log( stockCountInput );
     const stockCuzInput = document.querySelector('#stockCuzInput');     // console.log( stockCuzInput );
-    
+
     // 1-2. 입력 마크업 객체의 입력값 가져오기
-    const stock = stockInput.value;             console.log( stock );
-    const stockType = stockTypeInput.value;     console.log( stockType );
-    const stockCount = stockCountInput.value;   console.log( stockCount );
-    const stockCuz = stockCuzInput.value;       console.log( stockCuz );
+    const stock = stockInput.value; console.log(stock);
+    const stockType = stockTypeInput.value; console.log(stockType);
+    const stockCount = stockCountInput.value; console.log(stockCount);
+    const stockCuz = stockCuzInput.value; console.log(stockCuz);
     // 1-3. 유효성 검사
-    if ( stock == '' || stockType == '' || stockCount == '' ){
+    if (stock == '' || stockType == '' || stockCount == '') {
         alert('[오류] 비어있는 항목이 있습니다.');
         return;                         // 반환값 없는 함수 종료             
     }
     // 2. 어디에 : 제품 목록에
-    console.log( stock );
-    let pNum = getProductNum( stock );
-    console.log( pNum )
+    console.log(stock);
+    let pNum = getProductNum(stock);
+    console.log(pNum)
     // stype 얻는 함수
-    let stype = getstype( stockType );
-    console.log( stype );
+    let stype = getstype(stockType);
+    console.log(stype);
     // 2-1. 입력값들 객체화하기
     const obj = {
         sno: sno++,
@@ -33,12 +33,13 @@ function StockRegistration(){
         stype: stype,
         scount: stockCount,
         scuz: stockCuz
-    }; console.log( obj );
+    }; console.log(obj);
     // 2-2. 객체를 리스트에 넣기
-    stockList.push( obj );
-    console.log( stockList );
+    stockList.push(obj);
+    console.log(stockList);
+    stocksList();
     // 재고 목록 업데이트 함수 넣기
-    
+
     alert('재고 등록 성공')
 };
 
@@ -46,19 +47,17 @@ function StockRegistration(){
 //=============================변동명 옵션 함수=============================
 // 실행조건 : 페이지가 열릴 때, 제품이 등록/삭제 되었을 때
 stockTypeOption();
-function stockTypeOption(){
+function stockTypeOption() {
     // console.log('----stockTypeOption----');
     let html = `<option disabled selected> 제품명 </option>`;
     // html 넣을 객체 가져오기
     let stockInput = document.querySelector('#stockInput');
     // console.log( stockInput );
-    for ( let i = 0; i <= stockList.length - 1; i++){
-        let stock = stockList[i];
+    for (let i = 0; i <= productList.length - 1; i++) {
+        let stock = productList[i];
         // console.log( stock );
-        let product = getProductName( stock.pno )
-        // console.log(product)
         html += `<option>
-                    ${product}
+                    ${stock.pname}
                 </option>`
     }
     stockInput.innerHTML = html;
@@ -69,42 +68,105 @@ function stockTypeOption(){
 // 입고 유형
 let stype = true;
 stockOption();
-function stockOption(){
+function stockOption() {
     console.log('----stockOption----');
     let html = `<option disabled selected> 변동유형 </option>`;
     // html 넣을 객체 가져오기
     let stockTypeInput = document.querySelector('#stockTypeInput');
-    console.log( stockTypeInput );
+    console.log(stockTypeInput);
     html += `<option>입고</option>
             <option>출고</option>`
     stockTypeInput.innerHTML = html;
+    productsList();  // 제품 목록함수
 };
 
 //=============================제품코드로 제품이름 출력 함수=============================
 // 매개변수 : pno
-function getProductName( pno ){
-    for ( let i = 0; i <= productList.length - 1; i++){
+
+function getProductName(pno) {
+    for (let i = 0; i <= productList.length - 1; i++) {
         let product = productList[i];
-        if ( pno == product.pno ){
+        if (pno == product.pno) {
             return product.pname;
         }
     }
 };
 //=============================제품이름으로 제품코드 출력 함수=============================
 // 매개변수 : pname
-function getProductNum( pname ){
-    for ( let i = 0; i <= productList.length - 1; i++){
+function getProductNum(pname) {
+    for (let i = 0; i <= productList.length - 1; i++) {
         let product = productList[i];
-        if ( pname == product.pname ){
+        if (pname == product.pname) {
             return product.pno;
         }
     }
 }
 //=============================입고유형으로 true/false 출력 함수=============================
-function getstype( boolean ){
+function getstype(boolean) {
     // 입력값 객체 가져오기
     let stockTypeInput = document.querySelector('#stockTypeInput');
     let stockType = stockTypeInput.value;
     let stype = boolean == '입고' ? true : false;
     return stype;
 }
+
+// 2. 재고로그목록 출력함수
+stocksList();
+function stocksList() {
+    console.log('재고로그목록 출력함수 >>  stockList(); exe');
+    // 1/2. 마크업 객체/ 입력값 가져오기
+    const tbodyStock = document.querySelector('#stockTable tbody'); console.log(tbodyStock);
+
+    // 3. 상품명 추출과 제고 정보 데이터를 조회하기 위한 배열 순회
+    htmlStock = '';
+    for (let i = 0; i <= stockList.length - 1; i++) {  // 재고 배열
+        let stock = stockList[i]; console.log(stock);
+
+        let pStockName = '';
+        for (let i = 0; i <= productList.length - 1; i++) { // 상품 배열
+            let product = productList[i]; console.log(product);
+
+            if (product.pno === stock.pno) { // 재고 배열과 상품 배열의 상품 ID가 같다면 상품명 호출하여 pStockName 대입
+                pStockName = product.pname; console.log(product.pname);
+            }
+        }
+
+        htmlStock += `<tr>
+                    <td> ${pStockName} </td>
+                    <td> ${stock.stype ? '입고' : '출고'} </td>
+                    <td> ${stock.scount.toLocaleString()}  </td>
+                    <td> ${stock.scuz}  </td>
+                    <td> <button onclick="stockEdit( ${stock.sno})" > 입출사유변경 </button> </td>
+                </tr>`
+
+    }
+    tbodyStock.innerHTML = htmlStock;
+    // productRegistration(); // 재고로그 등록함수 호출
+    // stockEdit(sno); // 선택제품 함수 수정함수 호출
+
+} // productList(); end
+
+
+// 3. 재고로그목록 > 입출사유변경 수정 함수
+
+function stockEdit(sno) {   // 재고 코드를 매개함수로 입력하기
+    console.log(`stockEdit XXOK`)   // 확인
+    console.log(sno);
+    // (1) 재고 입출사유 수정할 번호의 객체를 찾는다. for문에서 if문으로 재고 코드가 찾는 재고 코드와 같은지 확인하기
+    for (let i = 0; i <= stockList.length - 1; i++) {
+        let stock = stockList[i];
+        if (stock.sno == sno) {    // 재고리스트 인덱스의 재고 코드가, 찾는 재고 코드와 같다면
+            const scuz = prompt('변경된 입출사유를 입력하세요.');
+            stock.scuz = scuz; // 프롬프트에 적은 문자열로 변경
+            alert('입출사유 수정 성공') // 안내
+            stocksList(); // 재고 로그 조회 새로고침
+            return; // 함수 종료
+        } // if end
+
+    } // for end
+    // (2) 입출사유 수정할 재고 번호가 없다면
+    alert('없는 재고 로그입니다.');
+
+}   // func end
+
+
